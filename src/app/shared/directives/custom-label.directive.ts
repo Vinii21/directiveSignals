0c3728ba282cbd209aadad3e2d8906ded6a12fb6
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-05-06 05:32:41 -0600

    Estructura para trabajar con signals, introduccion

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,16 +7,57 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement!: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
+  @Input() set color(value:string) {
+    this._color = value;
+    this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage()
+  }
 
   constructor( private el: ElementRef<HTMLElement>) {
-    console.log(el);
+    /* console.log(el); */
     this.htmlElement = el;
 
-    this.htmlElement.nativeElement.innerHTML = "Algo de información";
+    /* this.htmlElement.nativeElement.innerHTML = "Algo de información"; */
   }
 
   ngOnInit(): void {
     console.log('ng OnInit')
+  }
+
+  setStyle():void {
+    if( !this.htmlElement) return;
+    this.htmlElement!.nativeElement.style.color = this._color
+  };
+
+  setErrorMessage():void {
+    console.log(this._errors)
+    if ( !this.htmlElement) return;
+    if ( !this._errors) {
+      this.htmlElement.nativeElement.innerHTML = "";
+      return;
+    }
+    const errors = Object.keys(this._errors);
+
+    if(errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = "Este campo es requerido.";
+    }
+
+    if(errors.includes('minlength')) {
+      this.htmlElement.nativeElement.innerHTML = 'Necesita ' + this._errors['minlength']?.requiredLength + ' caracteres como minímo. Actualmente tienes ' + this._errors['minlength']?.actualLength;
+      return;
+    }
+
+    if(errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = "Necesita un formato de email valido";
+      return;
+    }
   }
 
 }
