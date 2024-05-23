b33bb9395c5052c7b5865b7c60881dbf221870ef
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-05-22 22:57:32 -0600

    Estructura de las mutaciones - señales de angular

--- src/app/signals/pages/properties-pages/properties-pages.component.ts
+++ src/app/signals/pages/properties-pages/properties-pages.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPagesComponent {
 
+  public onFieldUpdated(field: string, value: string) {
+    console.log({field, value})
+  }
+
 }
