dc36f54bc310496162badf5e69e9ec4aab2302c8
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-05-26 22:44:17 -0600

    Mutar o actualizar datos traidos de una API

--- src/app/signals/pages/properties-pages/properties-pages.component.ts
+++ src/app/signals/pages/properties-pages/properties-pages.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-pages.component.html',
@@ -6,8 +7,33 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPagesComponent {
 
-  public onFieldUpdated(field: string, value: string) {
-    console.log({field, value})
+  public user = signal<User>({
+    id: 1,
+    email: 'george.blueth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  });
+
+  public full_name = computed(()=> `${this.user().first_name} ${this.user().last_name}`);
+
+  public onFieldUpdated(field: keyof User, value: string) {
+    this.user.update(current => {
+      switch(field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+      }
+      return current;
+    })
   }
 
 }
